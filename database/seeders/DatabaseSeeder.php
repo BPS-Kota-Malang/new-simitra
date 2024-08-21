b740134836c2712055006e09caf3e83f54cdcb77
adeliaala <e31222707@student.polije.ac.id>
2024-08-21 09:05:30 +0700

    breeze login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+// boh su
 class DatabaseSeeder extends Seeder
 {
     public function run()
